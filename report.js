1cb75e5544825da61a774586c0ece99af2b53250
rajaul rabbi <Rajaul-Islam>
2023-08-27 22:08:44 +0600

    Readme

--- report.js
+++ report.js
@@ -5,7 +5,7 @@ newman.run(
     //collection: require('./collection/dmoney.json'),
     collection:
       "https://api.postman.com/collections/22666982-3374b582-2303-40c6-bdd5-044e1ab65fce?access_key=PMAT-01H8NQ1E3B23RD3CJ5GTZYNMCS",
-    // collection:"https://api.postman.com/collections/22666982-0cefb314-93c5-42b6-8137-806238ef0298?access_key=PMAT-01H8PREFGMSRMF5WX1J42PBQ3D",
+    
     //environment:require('./collection/env.json'),
     reporters: "htmlextra",
     iterationCount: 1,
